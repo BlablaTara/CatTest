ade245d1a2f468ff9f20a236f1ad9f24bf3dc6fc
BlablaTara <tara0561@stud.kea.dk>
2023-11-08 10:23:12 +0100

    CatTest

--- test/CatTest.java
+++ test/CatTest.java
@@ -1,4 +1,28 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CatTest {
-  
+    private Cat c;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        c = new Cat("Misser", 'M', 8);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+        assertNotNull(c.getName());
+        assertFalse(c.getName().isEmpty());
+    }
+
+    @Test
+    void getGender() {
+        assertTrue(c.getGender() == 'M'|| c.getGender() == 'F' );
+    }
+
+    @Test
+    void getWeightInKg() {
+        assertTrue(c.getWeightInKg() > 0 && c.getWeightInKg() <= 10);
+    }
 }
